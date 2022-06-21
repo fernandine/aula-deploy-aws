764b5f6be65fb813700f978593331ae8dae9db03
Jean Fernandine <jfernandine@gmail.com>
2022-06-21 14:37:35 -0300

    Resolvendo problema N+1 consultas

--- dscatalog-bds3-backend/src/main/java/com/devsuperior/dscatalog/repositories/ProductRepository.java
+++ dscatalog-bds3-backend/src/main/java/com/devsuperior/dscatalog/repositories/ProductRepository.java
@@ -19,4 +19,13 @@ public interface ProductRepository extends JpaRepository<Product, Long> {
 			+ "(:name = '' OR LOWER(obj.name) LIKE LOWER(CONCAT('%',:name, '%')))"
 			)
 	Page<Product> find(List<Category> categories, String name, Pageable pageable);
+	
+	/*
+	Resolvendo problema N+1 Lista de categorias;
+	OBS: JOIN FETCH não funciona com páginas, somente listas;
+	*/
+	
+	@Query("SELECT obj FROM Product obj JOIN FETCH obj.categories WHERE obj IN :products")
+	List<Product> findProductsWithCategories(List<Product> products);
+
 }
--- dscatalog-bds3-backend/src/main/java/com/devsuperior/dscatalog/services/ProductService.java
+++ dscatalog-bds3-backend/src/main/java/com/devsuperior/dscatalog/services/ProductService.java
@@ -37,7 +37,8 @@ public class ProductService {
 	 	List<Category> categories = (categoryId == 0) ? null :
 	Arrays.asList(categoryRepository.getOne(categoryId));
 	 	Page<Product> list = repository.find(categories, name, pageable);
-	 	return list.map(x -> new ProductDTO(x));
+	 	repository.findProductsWithCategories(list.getContent());
+	 	return list.map(x -> new ProductDTO(x, x.getCategories()));
 	}
 
 
